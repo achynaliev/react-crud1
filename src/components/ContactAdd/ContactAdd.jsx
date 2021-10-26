b17df63e4929a54e4188a4c5d034ea0c30f50a71
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-10-26 11:48:05 +0600

    added react bootstrap

---
+++ src/components/ContactAdd/ContactAdd.jsx
@@ -0,0 +1,14 @@
+import React, { createContext } from "react";
+// const Context = createContext([],
+//    addContact: (contact) => );
+
+const ContactAdd = () => {
+  return (
+    <div className="addCard">
+      <input type="text"></input>
+      <button></button>
+    </div>
+  );
+};
+
+export default ContactAdd;
